b499497c5e99fe31461c215f4c713594c27be8fc
Thomas Haller <hallert60@gmail.com>
2021-02-18 20:45:49 -0600

    added PlaylistHeader

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,13 @@
-import React from "react";
-import "../style/Body.css";
-import Header from "./Header";
+import React from 'react';
+import '../style/Body.css';
+import Header from './Header';
+import PlaylistHeader from './PlaylistHeader';
 
 const Body = () => {
 	return (
 		<div className="body">
 			<Header />
-			<div className="container">
-				<img
-					className="playlistImg"
-					src="https://i.scdn.co/image/ab67616d00001e02f337a21d945f44e802a1eb1d"
-					alt=""
-				/>
-				<div className="bodyItems">
-					<h1 className="playlistTitle">Playlist Title</h1>
-					<h4 className="songCount">150 songs</h4>
-				</div>
-			</div>
+			<PlaylistHeader className="playlistHeader" />
 		</div>
 	);
 };
---
+++ src/components/PlaylistHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/PlaylistHeader.css';
+
+const PlaylistHeader = () => {
+	return (
+		<div className="playlistHeader">
+			<img
+				className="playlistImg"
+				src="https://i.scdn.co/image/ab67616d00001e02f337a21d945f44e802a1eb1d"
+				alt=""
+			/>
+			<div className="bodyItems">
+				<h1 className="playlistTitle">Playlist Title</h1>
+				<h4 className="songCount">150 songs</h4>
+			</div>
+		</div>
+	);
+};
+
+export default PlaylistHeader;
