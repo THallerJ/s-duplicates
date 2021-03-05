94930e16128b90e4f167140405fa3d95e80eb108
Thomas Haller <hallert60@gmail.com>
2021-03-04 18:26:27 -0600

    now show dup tracks

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../style/Track.css';
 
 const Track = ({ track }) => {
 	return (
-		<div>
-			<h1>{track.track.name}</h1>
+		<div className="track">
+			<h1 className="title">{track.track.name}</h1>
+			<h1 className="title">artist</h1>
+			<h1 className="title">album</h1>
+			<h1 className="title">14:02</h1>
+			<h1 className="title">D</h1>
 		</div>
 	);
 };
---
+++ src/components/TracksHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TracksHeader = () => {
+	return <div></div>;
+};
+
+export default TracksHeader;
