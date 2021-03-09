90d9f6a7ed5317a56cfdd569feaecdc26ccd3742
Thomas Haller <hallert60@gmail.com>
2021-03-08 21:08:05 -0600

    can now remove tracks from saved tracks

--- src/components/Track.js
+++ src/components/Track.js
@@ -2,8 +2,12 @@ import React from 'react';
 import '../style/Track.css';
 import ClearIcon from '@material-ui/icons/Clear';
 import IconButton from '@material-ui/core/IconButton';
+import { removeSavedTrack } from '../spotify/tracks.js';
+import { GetUserContext } from '../context/UserContext';
 
 const Track = ({ track }) => {
+	const { token, currPlaylist } = GetUserContext();
+
 	const getTrackLength = (ms) => {
 		const min = Math.floor((ms / 1000 / 60) << 0);
 		const sec = Math.floor((ms / 1000) % 60);
@@ -20,7 +24,9 @@ const Track = ({ track }) => {
 
 			<IconButton
 				style={{ padding: '0px' }}
-				onClick={() => console.log('clicked')}
+				onClick={() => {
+					removeSavedTrack(token, track.track.id);
+				}}
 			>
 				<ClearIcon style={{ color: '#ffa4a2' }} />
 			</IconButton>
--- src/spotify/tracks.js
+++ src/spotify/tracks.js
@@ -8,6 +8,26 @@ export const getSavedTracks = async (token, offset) => {
 	return await getPaginatedResponse(url, token, limit, 0);
 };
 
+export const removeSavedTrack = async (token, id) => {
+	const response = await axios
+		.delete('https://api.spotify.com/v1/me/tracks', {
+			params: {
+				ids: id,
+			},
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		})
+		.then(async (resp) => {
+			return resp;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+	return response;
+};
+
 export const getSavedTracksInfo = async (token) => {
 	const response = await axios
 		.get('https://api.spotify.com/v1/me/tracks', {
