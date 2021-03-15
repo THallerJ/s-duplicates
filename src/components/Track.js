9def0b4e3fc5c72f6936a7b87a7fab55440e8959
Thomas Haller <hallert60@gmail.com>
2021-03-15 16:26:27 -0500

    now show all artist names in duplicate list

--- src/components/Track.js
+++ src/components/Track.js
@@ -44,10 +44,15 @@ const Track = ({ track }) => {
 	};
 
 	return (
-		// make sure to include all artists
 		<div className="track">
 			<h1 className="title">{track.track.name}</h1>
-			<h1 className="title">{track.track.artists[0].name}</h1>
+			<h1 className="title">
+				{track.track.artists
+					.map((artist) => {
+						return artist.name;
+					})
+					.join(', ')}
+			</h1>
 			<h1 className="title">{track.track.album.name}</h1>
 			<h1 className="title">{getTrackLength(track.track.duration_ms)}</h1>
 
