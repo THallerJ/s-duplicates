ecfd3ed5b420e89c76326178e6764b6b3d29e76e
Thomas Haller <hallert60@gmail.com>
2021-03-21 20:02:57 -0500

    changed ConfirmDialog style

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -5,9 +5,39 @@ import {
 	DialogContent,
 	DialogTitle,
 	DialogActions,
+	Grid,
+	Typography,
 } from '@material-ui/core';
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles({
+	root: {
+		justifyContent: 'center',
+	},
+	dialog: {
+		backgroundColor: 'black',
+	},
+	title: {
+		color: 'white',
+		fontSize: '1.5em',
+	},
+	content: {
+		color: 'white',
+		padding: '30px',
+	},
+	confirmButton: {
+		backgroundColor: '#76d275',
+	},
+	cancelButton: {
+		backgroundColor: '#ff6f60',
+	},
+	divider: {
+		background: '#282828',
+	},
+});
 
 const ConfirmDialog = ({
 	title,
@@ -17,18 +47,50 @@ const ConfirmDialog = ({
 	onConfirm,
 	onCancel,
 }) => {
+	const classes = useStyles();
 	return (
-		<Dialog open={open} onBackdropClick={() => setOpen(false)}>
-			<DialogTitle>{title}</DialogTitle>
-			<DialogContent>{content}</DialogContent>
-			<DialogActions>
-				<Button startIcon={<CheckIcon />} onClick={onConfirm}>
-					Confirm
-				</Button>
-				<Button startIcon={<ClearIcon />} onClick={onCancel}>
-					Cancel
-				</Button>
-			</DialogActions>
+		<Dialog
+			PaperProps={{
+				style: {
+					backgroundColor: '#121212',
+					justifyContent: 'center',
+				},
+			}}
+			open={open}
+			onBackdropClick={() => setOpen(false)}
+		>
+			<DialogTitle className={classes.title}>
+				<Typography align="center" variant="subtitle2">
+					{title}
+				</Typography>
+			</DialogTitle>
+
+			<Divider className={classes.divider} variant="middle" light={true} />
+
+			<DialogContent className={classes.content}>
+				<Typography align="center" variant="body1">
+					{content}
+				</Typography>
+			</DialogContent>
+
+			<Grid container alignItems="center" direction="column">
+				<DialogActions>
+					<Button
+						className={classes.confirmButton}
+						startIcon={<CheckIcon />}
+						onClick={onConfirm}
+					>
+						Confirm
+					</Button>
+					<Button
+						className={classes.cancelButton}
+						startIcon={<ClearIcon />}
+						onClick={onCancel}
+					>
+						Cancel
+					</Button>
+				</DialogActions>
+			</Grid>
 		</Dialog>
 	);
 };
